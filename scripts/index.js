a1baca55c0d6881a84c0774c09be4d080035e1c0
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-03-14 16:09:47 +0100

    extra 2 completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const numberExtBtn = document.getElementById("numberExtractionBtn")
 let arrayOfCellNumbers = []
 let arrayOfExtractedNumbers = []
+let arrayOfPlayerTables = []
 
 handleNumberExtraction = () => {
   let randomNumber = Math.ceil(Math.random() * 76)
@@ -8,10 +9,18 @@ handleNumberExtraction = () => {
   if (arrayOfExtractedNumbers.includes(randomNumber)) {
     console.log(randomNumber, "è già stato estratto")
   } else {
+    arrayOfPlayerTables.forEach((array) => {
+      array.forEach((number) => {
+        if (parseInt(number.innerText) === randomNumber) {
+          number.classList.add("selectedCell")
+        }
+      })
+    })
     arrayOfExtractedNumbers.push(randomNumber)
-    arrayOfCellNumbers[randomNumber - 1].style.backgroundColor = "black"
+    arrayOfCellNumbers[randomNumber - 1].classList.add("selectedCell")
   }
   console.log(arrayOfExtractedNumbers)
+  console.log(arrayOfPlayerTables)
 }
 
 const createTombolaTable = () => {
@@ -28,12 +37,39 @@ const createTombolaTable = () => {
     cellContainer.appendChild(cellNumber)
     tombolaContainer.appendChild(cellContainer)
   }
+}
 
-  // console.log(arrayOfCellNumbers)
+const createPlayerTable = (number) => {
+  const playerTables = document.getElementById("playerTables")
+
+  for (let j = 0; j < number; j++) {
+    const tablePlayerContainer = document.createElement("div")
+    tablePlayerContainer.classList.add("tablePlayerContainer")
+    const playerTablesNumbers = []
+    for (let i = 0; i < 24; i++) {
+      let randomNumber = Math.ceil(Math.random() * 76)
+
+      const cellContainer = document.createElement("div")
+      cellContainer.classList.add("cellContainer")
+
+      const cellNumber = document.createElement("h3")
+      cellNumber.classList.add("cellNumber")
+      cellNumber.innerText = randomNumber
+      playerTablesNumbers.push(cellNumber)
+
+      cellContainer.appendChild(cellNumber)
+      tablePlayerContainer.appendChild(cellContainer)
+    }
+    playerTables.appendChild(tablePlayerContainer)
+    arrayOfPlayerTables.push(playerTablesNumbers)
+  }
+
+  console.log(arrayOfPlayerTables)
 }
 
 window.onload = () => {
   numberExtBtn.addEventListener("click", handleNumberExtraction)
 
+  createPlayerTable(3)
   createTombolaTable()
 }
